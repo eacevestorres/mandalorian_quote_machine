4bf0bb114f86ea8f73b14b14f1efe301198e63e8
Eduardo Aceves <eduardoaceves99@gmail.com>
2020-08-10 21:32:38 -0500

    adding the machine

---
+++ quotes.js
@@ -0,0 +1,78 @@
+const QUOTEBANK = [
+  {
+    quote:"I can bring you in warm, or I can bring you in cold.",
+    author:"The Mandalorian"},
+  {
+    quote:"I have spoken.",
+    author:"Kuiil"},
+  {
+    quote:"I'm a Mandalorian. Weapons are part of my religion.",
+    author:"The Mandalorian"},
+  {
+    quote:"Our secrecy is our survival, our survival is our strength.",
+    author:"Armorer"},
+  {
+    quote:"When one chooses to walk the way of the mandalore, you are both hunter and prey.",
+    author:"Armorer"},
+  {
+    quote:"This is the way.",
+    author:"The Mandalorian"},
+  {
+    quote:"Droids are not good or bad. They are neutral reflections of those who imprint them.",
+    author:"Kuiil"},
+  {
+    quote:"I like those odds.",
+    author:"The Mandalorian"},
+  {
+    quote:"Mandalorian isn't a race. It's a creed.",
+    author:"The Mandalorian"},
+  {
+    quote:"I did disintegrate a few of them.",
+    author:"The Mandalorian"},
+  {
+    quote:"Do not self destruct. We're shooting our way out.",
+    author:"The Mandalorian"},
+  {
+    quote:"It was not a noble kill, he did not know he was my enemy",
+    author:"The Mandalorian"},
+  {
+    quote:"Look outside. Is the world more peaceful since the revolution? I see nothing but death and chaos.",
+    author:"The client"},
+  {
+    quote:"Hand over the child, Mando. I might let you live.",
+    author:"Riot Mar"},
+  {
+    quote:"The empire is gone, Mando. All that are left are mercenaries and warlords.",
+    author:"Greef Karga"},
+  {
+    quote:"You are a Mandalorian! Your ancestors rode the great mythosaur. Surely you can ride this young foal.",
+    author:"Greef Karga"},
+  {
+    quote:"The is nothing to be sad about. I have never been alive.",
+    author:"IG-11"},
+  {
+    quote:"The empire improves every system it touches. Judge by any metric. Safety, prosperity, trade, opportunity, peace.",
+    author:"The client"},
+  {
+    quote:"Such a large bounty for such a small package",
+    author:"The client"},
+  {
+    quote:"Come on, Do the magic hand thing... I'm out of ideas",
+    author:"Greef Karga"},
+  {
+    quote:"I'm no longer a hunter. I am a nurse android.",
+    author:"IG-11"},
+  {
+    quote:"Bounty hunting is a complicated profession.",
+    author:"The client"},
+  {
+    quote:"Your astute panic suggests that you understand your situation",
+    author:"Moff Gideon"},
+  {
+    quote:"Take some advice, kid. You wanna be a bounty hunter? Make the best deal for yourself and survive.",
+    author:"Fennec Shand"},
+  {
+    quote:"You know, you're not so bad for an android",
+    author:"The Mandalorian"},
+
+]
